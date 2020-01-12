58336fc25f214f178700478d3a4b54e93e9d2612
bambooka <onceup0nAtime>
2020-01-12 17:52:58 +0300

    add basic site structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import Header from './Header.js'
+// import Header from './Header.js'
 import ProjectList from './ProjectList.js'
 import './App.css';
+import {Layout, Header, Navigation, Content, Drawer} from "react-mdl";
+import Main from './components/Main';
+import {Link} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <ProjectList />
-
+      <div className="demo-big-content">
+        <Layout>
+          <Header title="Title" scroll>
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/about">About</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/about">About</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main/>
+          </Content>
+        </Layout>
+      </div>
     </div>
   );
 }

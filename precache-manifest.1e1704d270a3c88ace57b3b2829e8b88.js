6013c737f6298c52621a4da717c35e135db96140
bambooka <onceup0nAtime>
2020-05-30 17:46:42 +0300

    Updates

---
+++ precache-manifest.1e1704d270a3c88ace57b3b2829e8b88.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5ada6afbd558185d97e5c3cd5da911",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d25221a914d9c2a03af3",
+    "url": "/portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "610bd5d3c7f882fd6bdc",
+    "url": "/portfolio/static/css/main.632ee580.chunk.css"
+  },
+  {
+    "revision": "d25221a914d9c2a03af3",
+    "url": "/portfolio/static/js/2.0d80e8d1.chunk.js"
+  },
+  {
+    "revision": "610bd5d3c7f882fd6bdc",
+    "url": "/portfolio/static/js/main.0f91a848.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "8ad9576e4ec2d30718be0aa7853a7160",
+    "url": "/portfolio/static/media/recipe-site.8ad9576e.png"
+  },
+  {
+    "revision": "420dcfcf8cadb74ee4a4611e3e6297ce",
+    "url": "/portfolio/static/media/tech-store.420dcfcf.png"
+  },
+  {
+    "revision": "163cc187768db73a0c4aafe09f60f0cb",
+    "url": "/portfolio/static/media/todo.163cc187.png"
+  }
+]);
